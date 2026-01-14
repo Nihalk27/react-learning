a2954338244ed6018a27a6b811fa83351477c312
Nihalk27 <nihalkondukwar@gmail.com>
2026-01-14 09:15:46 +0530

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <h1>Learn Everyday Something New</h1>
       <p>it will be fun</p>
+      <p className="inline-block mt-8 w-fit px-2 py-1 border-1 rounded-xl ">Added the tailwind css to this </p>
     </>
   );
 }
